6cd568d61528accb0ac4016962eee3e0a8e133a4
alanhc <alanhc.tseng1999@gmail.com>
2020-07-27 18:09:34 +0800

    b

--- architecture0714/utils/color_filter.py
+++ architecture0714/utils/color_filter.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 from utils.files import getBaseName, createFolder
 state=''
-save = True
+save = False
 def Euclidean_filter(img, percent,up_threshold,lower_threshold,  color, img_BGR_spilt, save_path):
     filename = save_path.split('/')[2]
    
@@ -25,21 +25,22 @@ def Euclidean_filter(img, percent,up_threshold,lower_threshold,  color, img_BGR_
     d = (d_max-d_min)
     img_d = ((distance-d_min)/d*255.0).astype('uint8')
     
-    adaptive_mean = cv2.adaptiveThreshold(img_d, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 7,3)
-    adaptive_gaus = cv2.adaptiveThreshold(img_d, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7,3)
-    
-    t2, otsu = cv2.threshold(img_d, 0, 255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     
     if state == 'red':
-        threshold=170
+        threshold=60
     elif state == 'white':
         threshold=15
     
     #cv2.imshow("img_d", img_d)
-    #cv2.imshow("adaptive_mean", adaptive_mean)
-    #cv2.imshow("adaptive_gaus", adaptive_gaus)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
+    
+    print(filename)
+
+
+    idx = img_d[:,:] > threshold
+    img_filted = np.copy(img)
+    img_filted[idx] = 0
     if save:
         cv2.imwrite('img/out/'+filename+'/'+filename+'_'+state+'_img_distance_norm.png', img_d)
         cv2.imwrite('img/out/'+filename+'/'+filename+'_'+state+'_img_adaptive_gaus.png', adaptive_gaus)
@@ -47,28 +48,6 @@ def Euclidean_filter(img, percent,up_threshold,lower_threshold,  color, img_BGR_
         
         cv2.imwrite('img/out/'+filename+'/'+filename+'_'+state+'_img_otsu.png', otsu)
         
-        
-    """
-    plt.hist(distance, bins = 10)
-    plt.title(filename+' '+state+" histogram") 
-    plt.xlabel("value")
-    plt.ylabel("Frequency")
-    plt.savefig(save_path+filename+'_'+state+'_histogram.jpg')
-    """
-    """
-    print('=== '+state+' distance max ===',distance.max())
-    plt.hist(distance, bins = [0,20,40,60,80,100]) 
-    plt.title("histogram") 
-    plt.xlabel("value")
-    plt.ylabel("Frequency")
-    plt.savefig(save_path+state+'_histogram.jpg')
-    """
-    print(filename)
-
-
-    idx = distance[:,:] > threshold
-    img_filted = np.copy(img)
-    img_filted[idx] = 0
-
+       
     
     return img_filted, idx
--- architecture0714/utils/connect_compoent.py
+++ architecture0714/utils/connect_compoent.py
@@ -30,7 +30,7 @@ def find_BoundingBox(img, img_origin):
         h_max = i[-1]
         w_min = j[0]
         w_max = j[-1]
-        cv2.rectangle(img_origin, (w_min,h_min), (w_max, h_max), (0,0,255), 2)
+        cv2.rectangle(img_origin, (w_min,h_min), (w_max, h_max), (255,0,0), 2)
         
         np.append(boxes, np.array([[w_min,h_min, w_max, h_max]]), axis=0)
         #print(index,boxes[i])
